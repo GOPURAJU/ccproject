9c12b2e42e1bebb2e1485d812ee246160615815d
GOPURAJU <100461013+GOPURAJU@users.noreply.github.com>
2024-07-30 10:57:05 +0530

    Update dbconnection.php

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "crmsdb");
+$con=mysqli_connect("http://database-3.ch0ymcuikejv.eu-north-1.rds.amazonaws.com/", "admin", "cc12345678", "ccproject");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
