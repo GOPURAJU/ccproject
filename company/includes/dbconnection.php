b096244bf780161ff9fac699769a98da7aa63597
GOPURAJU <100461013+GOPURAJU@users.noreply.github.com>
2024-07-30 15:00:54 +0530

    Update dbconnection.php

--- company/includes/dbconnection.php
+++ company/includes/dbconnection.php
@@ -5,7 +5,5 @@ ini_set('display_errors', 1);
 $con = mysqli_connect("database-3.ch0ymcuikejv.eu-north-1.rds.amazonaws.com", "admin", "cc12345678", "ccproject");
 if (mysqli_connect_errno()) {
     echo "Connection Failed: " . mysqli_connect_error();
-} else {
-    echo "Connected successfully!";
-}
+} 
 ?>
